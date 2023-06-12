909208313935d57119948ce915dd9db1b1af18a7
Brendon Mascarenhas <brendon_genssinger@hotmail.com>
2023-06-12 20:34:06 -0300

    Inclusão da Terceira Section O que é a SaferMKT

---
+++ src/app/pagesSafer-mkt/component/buttons.tsx
@@ -0,0 +1,18 @@
+
+export default function WhatsAppButtonClient(){
+
+    return (
+        <div className="btn btn-info">
+            <a href="https://wa.me/message/AGDHQJYF43H5G1" 
+                title="Share on whatsapp" 
+                target="_blank"
+                style={{}}>WhatsApp</a>
+        </div>        
+        
+    );
+  };
+  
+;
+
+
+
